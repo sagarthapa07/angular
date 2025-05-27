0e16c14b8e4ed7c873edff7a072145d9711ca766
sagar <sagarthapa7757@gmail.com>
2025-05-27 07:52:06 +0530

    Login Form (Reactive)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { ResourceComponent } from "./components/resource/resource.component";
 import { SignUpComponent } from "./components/sign-up/sign-up.component";
 import { GrantComponent } from "./components/grant/grant.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ResourceComponent, SignUpComponent, GrantComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ResourceComponent, SignUpComponent, GrantComponent, LoginComponent],
   templateUrl: './app.component.html',
   //styleUrl: './app.component.css'
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,28 +1,47 @@
 import { Component } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormsModule,
+  FormGroup,
+  ReactiveFormsModule,
+  
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  email = 'sagar@gmail.com';
-  password = '1234';
-
-  ngOnInit() {
-    console.log('Hi Sagar');
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
-  }
-
-  onSubmit() {
-    console.log('Hi Vikalp');
-    this.email = "vikalp"
-    this.password = "qwerty"
-    console.log('Email:', this.email);
-    console.log('Password', this.password);
+  loginForm : FormGroup
+  ngOnInit(){
+    let email = new FormControl()
+    let password= new FormControl()
+    this.loginForm = new FormControl({
+      email: email,
+      password: password,
+    });
   }
 }
+// email = 'sagar@gmail.com';
+// password = '1234';
+
+// ngOnInit() {
+//   console.log('Hi Sagar');
+//   console.log('Email:', this.email);
+//   console.log('Password:', this.password);
+// }
+
+// onSubmit() {
+//   console.log('Hi Vikalp');
+//   this.email = 'vikalp';
+//   this.password = 'qwerty';
+//   console.log('Email:', this.email);
+//   console.log('Password', this.password);
+// }
+
+// form = new FormGroup({
+//   username: new FormControl('', Validators.required),
+// });
