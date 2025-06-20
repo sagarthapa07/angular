f9c7b550d7c2113c745ff0025f3eb9b2afab0b81
sagarthapa07 <sagarthapa7757@gmail.com>
2025-06-20 10:56:31 +0530

    created cart file and updated detail fle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { GrantComponent } from './components/grant/grant.component';
 import { LoginComponent } from './components/login/login.component';
 import { DetailComponent } from './components/detail/detail.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
 
 
 export const routes: Routes = [
@@ -17,4 +18,5 @@ export const routes: Routes = [
 {path:"grant", component:GrantComponent},
 {path:"Login", component:LoginComponent},
 {path:"detail", component:DetailComponent},
+{path:"cartPage", component:CartPageComponent},
 ];
