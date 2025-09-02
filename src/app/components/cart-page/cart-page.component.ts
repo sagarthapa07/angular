2edf3d0d4f52863af1d0d9f4c739a5f76eb158c5
sagarthapa07 <sagarthapa7757@gmail.com>
2025-09-02 10:45:25 +0530

    Refactor cart and header components for improved functionality and UI consistency

--- src/app/components/cart-page/cart-page.component.ts
+++ src/app/components/cart-page/cart-page.component.ts
@@ -1,95 +1,53 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { CartService } from '../../core/services/cart/cart.service';
-
 @Component({
   selector: 'app-cart-page',
   imports: [RouterLink, ],
   templateUrl: './cart-page.component.html',
-  styleUrl: './cart-page.component.css'
+  styleUrl: './cart-page.component.css',
 })
-export class CartPageComponent {
- 
-  count = 1;
+export class CartPageComponent implements OnInit {
+  cartArr: any[] = [];
+  total: any;
 
-  handleCounter(val: string) { 
-    if(val == 'sub') {
-      if(this.count > 0){
-        this.count = this.count-1
-      }else{
-        this.count=0
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.fetchCartData().subscribe((res) => {
+      const data = res.products.map((item: any) => ({
+        id: item.id,
+        img: item.thumbnail,
+        name: item.title,
+        Variant: 'Default',
+        status: 'In Stock',
+        price: item.discountedPrice || item.total,
+        quantity: item.quantity,
+      }));
+      this.cartArr = data;
+    });
+  }
+
+  handleCounter(type: string, id: string, quantity: number) {
+    const qty = type === 'plus' ? quantity + 1 : quantity - 1;
+    this.cartService.cartAddItem(id, qty).subscribe((res: any) => {
+      if (res) {
+      this.cartArr =  this.cartArr.map((val)=>{
+        if(id == val.id){
+         return {
+            ...val,
+            quantity: res.products[0].quantity
+          }
+        }
+        return val
+       })
       }
-    }else if (val == 'plus'){
-      this.count = this.count+1
-    }else{
-      this.count = 0
-    }
+    });
   }
-  cartService = inject(CartService);
-  cartArr = this.cartService.cartItems;
-  total = this.cartService.totalAmount;
-
-  constructor() {
-    // call API on component load
-
+  price(id:string, quantity:number, price:number){
+    this.cartService.cartAddItem(id, price).subscribe((resPrice:any)=>{
+      debugger
+    })
   }
-
-
-ngOnInit(): void {
-      this.cartService.fetchCartData();
-//   this.cartService.getCartData().subscribe((res) => {
-//     this.cartService = res.products.map((item: any) => ({
-//       img: item.thumbnail,
-//       name: item.title,
-//       Variant: 'Default',
-//       status: 'In Stock',
-//       price: item.discountedPrice || item.total,
-//       quantity: item.quantity,
-//     }));
-//   });
-}
-  
-  // cartArr = [
-  //   {
-  //     prodID: 1,
-  //     img: 'https://shopcart.reactbd.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Foe6m9gg8%2Fproduction%2Fc6e0da93e884e6ab9fc7b2a884c6d28ad7f2020e-500x500.png&w=750&q=75',
-  //     name:'Canon EOS 250D 24.1MP Full HD WI-FI DSLR Camera with 18-55mm',
-  //     Variant: 'Gadget',
-  //     status: 'Sale',
-  //     price: 750.00,
-  //   },
-  //   {
-  //     prodID: 1,
-  //     img: 'https://shopcart.reactbd.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Foe6m9gg8%2Fproduction%2Fc6e0da93e884e6ab9fc7b2a884c6d28ad7f2020e-500x500.png&w=750&q=75',
-  //     name:'Canon EOS 250D 24.1MP Full HD WI-FI DSLR Camera with 18-55mm',
-  //     Variant: 'Gadget',
-  //     status: 'Sale',
-  //     price: 750.00,
-  //   },
-  //   {
-  //     prodID: 1,
-  //     img: 'https://shopcart.reactbd.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Foe6m9gg8%2Fproduction%2Fc6e0da93e884e6ab9fc7b2a884c6d28ad7f2020e-500x500.png&w=750&q=75',
-  //     name:'Canon EOS 250D 24.1MP Full HD WI-FI DSLR Camera with 18-55mm',
-  //     Variant: 'Gadget',
-  //     status: 'Sale',
-  //     price: 750.00,
-  //   },
-  //   {
-  //     prodID: 1,
-  //     img: 'https://shopcart.reactbd.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Foe6m9gg8%2Fproduction%2Fc6e0da93e884e6ab9fc7b2a884c6d28ad7f2020e-500x500.png&w=750&q=75',
-  //     name:'Canon EOS 250D 24.1MP Full HD WI-FI DSLR Camera with 18-55mm',
-  //     Variant: 'Gadget',
-  //     status: 'Sale',
-  //     price: 750.00,
-  //   },
-  //   {
-  //     prodID: 1,
-  //     img: 'https://shopcart.reactbd.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Foe6m9gg8%2Fproduction%2Fc6e0da93e884e6ab9fc7b2a884c6d28ad7f2020e-500x500.png&w=750&q=75',
-  //     name:'Canon EOS 250D 24.1MP Full HD WI-FI DSLR Camera with 18-55mm',
-  //     Variant: 'Gadget',
-  //     status: 'Sale',
-  //     price: 750.00,
-  //   },
-  // ]
 }
