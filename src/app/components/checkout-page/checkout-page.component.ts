5f856a4e36854e5bdc942212ab2d7e2017982143
Sagar <sagarthapa7757@gmail.com>
2025-11-21 17:26:24 +0530

    popup from

--- src/app/components/checkout-page/checkout-page.component.ts
+++ src/app/components/checkout-page/checkout-page.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-checkout-page',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './checkout-page.component.html',
   styleUrl: './checkout-page.component.css'
 })
 export class CheckoutPageComponent {
+showAddressPopup = false;
+
+openLoginForm() {
+  this.showAddressPopup = true;
+}
+
+closeAddressForm() {
+  this.showAddressPopup = false;
+}
+
+saveAddress() {
+  alert("Address Saved Successfully!");
+  this.closeAddressForm();
+}
 
 }
