050f3775a6c71aa2e4760368097a29415ae7eeb1
Sagar <sagarthapa7757@gmail.com>
2025-12-02 17:33:24 +0530

    dashbord section updated

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,28 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgbModule, NgbNavItem, NgbNavOutlet } from '@ng-bootstrap/ng-bootstrap';
+
+// import { NgbModule, NgbNavItem, NgbNavOutlet } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-profile',
-  imports: [NgbNavItem, NgbNavOutlet,NgbModule],
+  imports: [NgFor],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-active = 1;
+menuItems = [  
+  { name: 'Dashboard', icon: 'bi bi-columns-gap', route: 'main' },
+  { name: 'Orders', icon: 'bi bi-box-seam', route: 'orders' },
+  { name: 'Wishlist', icon: 'bi bi-heart', route: 'wishlist' },
+  { name: 'Addresses', icon: 'bi bi-geo-alt', route: 'addresses' },
+  { name: 'Personal Info', icon: 'bi bi-person-fill', route: 'personal-info' },
+  ];
+activeItem = 'Dashboard'; 
+
+
+
+
+
+
+
 }
